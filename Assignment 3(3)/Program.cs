f8b610ecf10855538e8eb8107513b8bfeda09e89
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:11:27 +0530

    Add project files.

---
+++ Assignment 3(3)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using AssemblyA;
+
+[assembly: InternalsVisibleTo("AssemblyB")]
+
+namespace AssemblyA
+{
+    internal class MyClass
+    {
+        internal int internalField = 58;
+
+        internal void DisplayInternalField()
+        {
+            Console.WriteLine("Internal Field from Assembly A: " + internalField);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        Console.WriteLine("Accessing Internal Field from Assembly B: " + myObject.internalField);
+        myObject.DisplayInternalField();
+    }
+}
